280f47c4d4b235d56bcc70145033bc51f7643340
jasmika-prajapati <jasmika.j.prajapati@gmail.com>
2024-07-04 20:22:59 -0400

    Update test.spec.js

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,9 +1,9 @@
-const { chromium } = require('playwright');
+const { firefox } = require('playwright');
 const { test, expect } = require('@playwright/test');
 
 test.describe('My Test Suite', () => {
   test('My Test Case', async ({}) => {
-  const browser = await chromium.launch();
+  const browser = await firefox.launch();
   const page = await browser.newPage();
   
   await authenticate(page);
